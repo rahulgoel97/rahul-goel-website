8daa71627cef95ed07a4997ed9c6621b5b21c016
Rahul <rahulgoel2019@u.northwestern.edu>
2021-10-27 20:50:11 -0500

    👀 fixed mistakes

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ function App() {
         <h1 className="greeting-text">
           😁👋 Hi, I'm Rahul  
         </h1>
-
-        <p className="main-page-sub-p"> This website is under development. </p>
+        <p className="main-page-sub-p"> I'm a corporate strategy professional with interests in critical networks, web3 & XR. Excited by technology that can dramatically reduce cost and improve access. </p>
+        <p className="main-page-sub-p  "> This website is under development. </p>
         
          <p className="main-page-p"> Music 🎵 </p>
          <p className="main-page-p"> Software 👨🏽‍💻 </p>
          <p className="main-page-p"> Pictures 👀</p>
-         <p className="main-page-p"> Blog 💭</p>
+         <p className="main-page-p"> <a href = "https://mirror.xyz/0x1426FAF5B6F335313ddEce3D5d4aAdD39525e44d/wzsIaOk8uOe6CFIAxthNCt9q43H_mbXJ9MsSaigIhcQ">Blog 💭</a></p>
          <Socials />
 
 
