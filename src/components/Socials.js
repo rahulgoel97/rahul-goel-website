8a411acd2f37748694d3b612f38d65a1dfb9067d
Rahul Goel <RTBF73@motorolasolutions.com>
2021-11-23 16:55:00 -0500

    Add social links and icons

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { SocialIcon } from 'react-social-icons';
 import '../App.css';
+import icongithub from './github-icon.png';
+import iconbandcamp from './bandcamp-icon.png';
+import iconsoundcloud from './soundcloud-icon.png';
+import icontwitter from './twitter-icon.png';
+import iconemail from './email-icon.png';
+import iconlinkedin from './linkedin-icon.png';
+
+
+
 
 
 function Socials() {
@@ -8,14 +18,48 @@ function Socials() {
     <div className="social-box">
       
       
-      <p> 
-      <a href="https://github.com/rahulgoel97">		GitHub		</a>
-      <a href="https://bandcamp.com/rahulgoel">		Bandcamp		</a>
-      <a href="https://tinyurl.com/linkrahul">		LinkedIn		</a>
-      </p>
+      <a href="https://github.com/rahulgoel97">
+      		<img 
+      		src={icongithub}
+      		style={{width:'2rem',margin:'0.5rem'}}
+      		/>
+      </a>
 
-      
+      <a href="https://bandcamp.com/rahulgoel"> 
+      		<img 
+      		src={iconbandcamp}
+      		style={{width:'3rem',margin:'0.5rem'}}
+      		/>
+      </a>
 
+      <a href="https://tinyurl.com/shivxbigother">
+      		<img 
+      		src={iconsoundcloud}
+      		style={{width:'2rem',margin:'0.5rem'}}
+      		/>
+      </a>
+
+
+      <a href="https://twitter.com/rahulxgoel">
+      		<img 
+      		src={icontwitter}
+      		style={{width:'2.2rem',margin:'0.6rem'}}
+      		/>
+      </a>
+
+      <a href="mailto:rahulgol97@gmail.com">
+      		<img 
+      		src={iconemail}
+      		style={{width:'1.9rem',margin:'0.7rem'}}
+      		/>
+      </a>
+
+       <a href="https://tinyurl.com/linkrahul">
+       		<img 
+      		src={iconlinkedin}
+      		style={{width:'2rem',margin:'0.5rem'}}
+      		/>
+      </a>  
     </div>
   );
 }
