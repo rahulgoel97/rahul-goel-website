5a5ed1ca004ca52a95c8b010773a06a711f51016
Rahul Goel <rahulgoel2019@u.northwestern.edu>
2021-12-02 17:11:05 -0600

    Added StressCalc images

--- src/StressCalc.js
+++ src/StressCalc.js
@@ -4,6 +4,12 @@ import './App.css';
 import SocialMediaButtons from 'react-social-media-buttons';
 import Socials from './components/Socials.js'
 import NavbarComponent from './components/NavbarComponent.js';
+import screen1 from './components/Screen1.png';
+import screen2 from './components/Screen2.png';
+import screen3 from './components/Screen3.jpg';
+import screen4 from './components/Screen4.png';
+import screen5 from './components/Screen5.jpg';
+
 
 function StressCalc() {
 
@@ -12,13 +18,37 @@ function StressCalc() {
     
         <NavbarComponent />  
 
-        <div className="page-title"> 
-             StressCalc 👀
+        <div className="page-title-stresscalc"> 
+             StressCalc 
         </div>   
 
-        <p className="main-page-sub-p  "> Coming Soon! </p>
+        <p className="main-page-sub-p-stresscalc  "> Banish math in the gym </p>
+        <img
+            className="stresscalc-screenshots" 
+            src={screen1}
+            />
 
-      <Socials />
+        <img
+            className="stresscalc-screenshots"
+            src={screen2}
+            />
+
+        <img
+            className="stresscalc-screenshots"
+            src={screen3}
+            />
+
+        <img
+            className="stresscalc-screenshots"
+            src={screen4}
+            />
+
+        <img
+            className="stresscalc-screenshots"
+            src={screen5}
+            />
+             
+     <Socials />
 
     </div>
   );
