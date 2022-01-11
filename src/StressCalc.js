c3a4bfb498c50b5107329a9b776d8abde9e3e6e5
Rahul Goel <rahulgol97@gmail.com>
2022-01-11 10:19:09 -0600

    Add ProductHunt badge to StressCalc

--- src/StressCalc.js
+++ src/StressCalc.js
@@ -36,6 +36,10 @@ function StressCalc() {
         <a href="https://play.google.com/store/apps/details?id=com.goellabs.stresscalc&hl=en_US&gl=US"><img
             style={{height:'8vmin'}}
             src={googlestore}/></a>
+
+        <a href="https://www.producthunt.com/posts/stresscalc?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-stresscalc">
+            <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=326328&theme=dark" alt="StressCalc - Minimal RPE calculator for lifters | Product Hunt" 
+            style={{height:'5.5svmin'}}/></a>
     
         </div>
 
