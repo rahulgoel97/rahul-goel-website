43261ebfd8089a501d8bed9f41570ea8b669bb28
Rahul Goel <rahulgol97@gmail.com>
2022-02-05 14:38:36 -0600

    Added quote and gif

--- src/Ideas.js
+++ src/Ideas.js
@@ -5,7 +5,20 @@ import SocialMediaButtons from 'react-social-media-buttons';
 import Socials from './components/Socials.js'
 import NavbarComponent from './components/NavbarComponent.js';
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 function Ideas() {
+
+  const quote={
+                body:[
+                  {quote: "Instead of patiently writing out a complete program and assuring myself it was correct, I tended to just spew out code that was hopelessly broken...For a long time I felt bad about this, just as I once felt bad that I didn't hold my pencil the way they taught me to in elementary school. Only if I had looked at other makers, the painters, or the architects, I would have realized there was a name for what I was doing: sketching", 
+                  author: "Paul Graham, Hackers & Painters"},
+    ]
+  }
+
+console.log(quote)
 
   return (
     <div className="App">
@@ -13,12 +26,18 @@ function Ideas() {
         <NavbarComponent />  
 
         <div className="page-title"> 
+            Quote
+        </div>  
+
+        <p className="main-page-sub-p  "> "{quote.body[0].quote}"</p>
+        <p className="main-page-sub-p  "> -{quote.body[0].author} </p>
+
+
+        <div className="page-title"> 
              Ideas 😲
         </div>   
 
-        <p className="main-page-sub-p  "> I hunt for the most original, thought provoking & contrarian ideas. Here are ideas that have impacted my thinking, summarized here for your pleasure. 
-<br/><br/>
-        Upcoming: Notes on Nicholas Nassim Taleb, Peter Theil (vs Marc Andreesen, vs David Graeber), Paul Graham, Ruth Porat, David Graeber, Justice Scalia, Keith Rabois, Ried Hoffman, Mellody Hobson, Chomsky, Damodaran.  </p>
+        <p className="main-page-sub-p  "> Upcoming: Notes on Nicholas Nassim Taleb, Peter Theil (vs Marc Andreesen, vs David Graeber), Paul Graham, Ruth Porat, David Graeber, Justice Scalia, Keith Rabois, Ried Hoffman, Mellody Hobson, Chomsky, Damodaran.  </p>
 
       <Socials />
 
--- src/Metaverse.js
+++ src/Metaverse.js
@@ -4,6 +4,7 @@ import './App.css';
 import SocialMediaButtons from 'react-social-media-buttons';
 import Socials from './components/Socials.js'
 import NavbarComponent from './components/NavbarComponent.js';
+import goellabsgif from './components/goellabs.gif'
 
 function Pics() {
 
@@ -20,6 +21,9 @@ function Pics() {
 <br/><br/>
 I'm deeply excited by what virtual and augmented reality can do for our world, and I cannot help but think about how I can contribute to building it.</p>
 
+    <p className="main-page-sub-p  "> A flask, representing experiments, made in Blender </p>
+    <img src = {goellabsgif} width="50%"/>
+    
       <Socials />
 
     </div>
