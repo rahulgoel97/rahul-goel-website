7ad38c75de03e19c8298585da065385cf04b5bc9
Rahul Goel <rahulgol97@gmail.com>
2022-02-12 13:25:33 -0600

    Updated projects

--- src/Projects.js
+++ src/Projects.js
@@ -7,6 +7,8 @@ import NavbarComponent from './components/NavbarComponent.js';
 import ProjectCard from './components/ProjectCard.js';
 import { Link } from 'react-router-dom';
 import icon from './components/icon.png';
+import gv from './components/gv.png';
+
 function Projects() {
 
   return (
@@ -27,7 +29,25 @@ function Projects() {
                                                 margin: '1rem'
                                                }}        
                                     /></Link>
-        </div>  
+            <div className="project-summary">
+                    StressCalc, an app for beginner powerlifters
+            </div>
+        </div>
+
+        <div className="project-card-grid">
+            <a href= "https://rahulgoel97.github.io/gamestonk-valuation/">
+                                    <img
+                                        src={gv}
+                                        style={{width: '20vmin',
+                                                borderRadius: '10px',
+                                                margin: '1rem'
+                                              }}
+                                        /></a>
+            <div className="project-summary">
+                    Gamestonk Valuation, a DCF app to value gamestop shares
+            </div>
+        </div>
+              
 
         <Socials />
 
