1616b9887a5d52fc2b6822c12de91ebc6aca0a1c
Rahul Goel <rahulgol97@gmail.com>
2022-08-21 17:38:54 -0500

    added pic

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import './App.css';
 import SocialMediaButtons from 'react-social-media-buttons';
 import Socials from './components/Socials.js'
 import NavbarComponent from './components/NavbarComponent.js';
+import Selfie from './components/selfie.jpg'
 
 
 function Home() {
@@ -13,10 +14,18 @@ function Home() {
     
         <NavbarComponent />    
         <div className="home-contents"> 
-            <h1 className="greeting-text">
-             😁👋 Hi, I'm Rahul  
-           </h1>
-           <p className="main-page-sub-p"> I'm a business strategist living in Chicago. I often think about how technology can dramatically reduce cost, improve access and forge  a path for self-actualization for everyone.</p>
+
+
+            <div className="home-intro-box">
+                <img src={Selfie} className="selfie"/>
+                <h1 className="greeting-text">
+                       😁👋 Hi, I'm Rahul 
+                </h1>
+
+            </div>
+
+
+           <p className="main-page-sub-p"> I currently live in Chicago and work at Motorola Solutions. I grew up in India and previously studied at Northwestern University. I often think about how technology can dramatically reduce cost, improve access and forge  a path for self-actualization for everyone.</p>
            <p className="main-page-sub-p  ">Interests: powerlifting, dance music, cooking</p>
            <p className="main-page-sub-p  ">Aspiration: become a polymath</p>
            <p className="main-page-sub-p  ">Building: apps to make my life easier & have some fun</p>
