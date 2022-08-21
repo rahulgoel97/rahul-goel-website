59dda5ecb35bfbed691f84b7953287e5c0637c2e
Rahul Goel <rahulgol97@gmail.com>
2022-08-21 16:56:42 -0500

    CSS changes

--- src/Ideas.js
+++ src/Ideas.js
@@ -13,14 +13,26 @@ function Ideas() {
 
   const quote={
                 body:[
-                  {quote: "Instead of patiently writing out a complete program and assuring myself it was correct, I tended to just spew out code that was hopelessly broken...For a long time I felt bad about this, just as I once felt bad that I didn't hold my pencil the way they taught me to in elementary school. Only if I had looked at other makers, the painters, or the architects, I would have realized there was a name for what I was doing: sketching", 
-                  author: "Paul Graham, Hackers & Painters"},
-                  {quote: "The cultural, or institutional rule is: no polymaths allowed. You can be narrowly specialized, and if you are interested in other things, you better keep it to youself and not tell people... because if say you are interested in computer science and also music or studying the Hebrew bible, well, that must mean you are just not very serious about computer science", 
-                  author: "Peter Theil, The Portal"},
+                  {quote: 
+                      "Instead of patiently writing out a complete program and assuring myself it was correct, I tended to just spew out code that was hopelessly broken...For a long time I felt bad about this, just as I once felt bad that I didn't hold my pencil the way they taught me to in elementary school. Only if I had looked at other makers, the painters, or the architects, I would have realized there was a name for what I was doing: sketching", 
+                  author: 
+                      "Paul Graham, Hackers & Painters"},
+
+
+                  {quote: 
+                    "The cultural, or institutional rule is: no polymaths allowed. You can be narrowly specialized, and if you are interested in other things, you better keep it to youself and not tell people... because if say you are interested in computer science and also music or studying the Hebrew bible, well, that must mean you are just not very serious about computer science", 
+                  author: 
+                    "Peter Theil, The Portal"},
+
+
+                  {quote: 
+                    "If you're in the US and go to a good school, there are a lot of forces that will push you towards following traintracks laid by others rather than charting a course yourself. Make sure that the things you're pursuing are weird things that you want to pursue, not whatever the standard path is. Heuristic: do your friends at school think your path is a bit strange? If not, maybe it's too normal.",
+                  author: 
+                    "Patrick Collison"},
     ]
   }
 
-var indexValue = getRandomInt(2)
+var indexValue = getRandomInt(3)
 console.log("Congratulations, you have found all the quotes I have here...")
 console.log(quote)
 
--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -25,9 +25,8 @@ function NavbarComponent() {
     <Nav className="me-auto">
       <Nav.Link href="#music">Music 🎵</Nav.Link>
       <Nav.Link href="#projects">Software 👨🏽‍💻</Nav.Link>
-      <Nav.Link href='#pics'>Metaverse 👀</Nav.Link>
-      <Nav.Link href="https://mirror.xyz/0x1426FAF5B6F335313ddEce3D5d4aAdD39525e44d/wzsIaOk8uOe6CFIAxthNCt9q43H_mbXJ9MsSaigIhcQ">Blog 💭</Nav.Link>
-      <Nav.Link href='#ideas'>Idea Hunt 😲</Nav.Link>
+      <Nav.Link href='#ideas'>Ideas 🤯 </Nav.Link>
+      <Nav.Link href="https://mirror.xyz/0x1426FAF5B6F335313ddEce3D5d4aAdD39525e44d/wzsIaOk8uOe6CFIAxthNCt9q43H_mbXJ9MsSaigIhcQ"> Charter 💭</Nav.Link>
     </Nav>
   </Navbar.Collapse>
 
