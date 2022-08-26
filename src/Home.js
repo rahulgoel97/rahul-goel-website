a14d99a4749a2f95ce1f9b2ff2f92bf13e24845e
Rahul Goel <rahulgol97@gmail.com>
2022-08-26 14:59:52 -0500

    Minor css changes

--- src/Home.js
+++ src/Home.js
@@ -24,12 +24,13 @@ function Home() {
 
             </div>
 
+            <div className="main-page-p-box">
+                <p className="main-page-sub-p"> I currently live in Chicago and work at Motorola Solutions. I grew up in India and previously studied at Northwestern University. I often think about how technology can dramatically reduce cost, improve access and forge  a path for self-actualization for everyone.</p>
+                <p className="main-page-sub-p  ">Interests: powerlifting, dance music, cooking</p>
+                <p className="main-page-sub-p  ">Aspiration: become a polymath</p>
+                <p className="main-page-sub-p  ">Building: apps to make my life easier & have some fun</p>
+            </div>
 
-           <p className="main-page-sub-p"> I currently live in Chicago and work at Motorola Solutions. I grew up in India and previously studied at Northwestern University. I often think about how technology can dramatically reduce cost, improve access and forge  a path for self-actualization for everyone.</p>
-           <p className="main-page-sub-p  ">Interests: powerlifting, dance music, cooking</p>
-           <p className="main-page-sub-p  ">Aspiration: become a polymath</p>
-           <p className="main-page-sub-p  ">Building: apps to make my life easier & have some fun</p>
-             
         </div>     
       <Socials />
 
