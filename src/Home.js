b317f68ee084c5c0ae8c3cf6438cdf4388f417c4
Rahul Goel <rahulgol97@gmail.com>
2024-07-13 14:03:32 -0500

    typo fix

--- src/Home.js
+++ src/Home.js
@@ -27,7 +27,7 @@ function Home() {
 
             <div className="main-page-p-box">
                 <p className="main-page-sub-contact"> (416) 561 8175 | rahulgol97@gmail.com </p>
-                <p className="main-page-sub-p"> I'm a strategy professional based in Chicago, currently at Motorola Solutions. I focus on research & quantiative analysis for strategic projects and tech M&A. </p>
+                <p className="main-page-sub-p"> I'm a strategy professional based in Chicago, currently at Motorola Solutions. I focus on research & quantitative analysis for strategic projects and tech M&A. </p>
                 <p className="main-page-sub-p  "> I grew up in New Delhi & Mumbai, studied engineering at Northwestern University and have lived in Chicago since 2015. I often think about how technology can dramatically reduce cost, improve access and forge a path for self-actualization for everyone.  </p>
                 <p className="main-page-sub-p  ">Interests: <a href="https://www.instagram.com/p/C2Tird-rOqa/">powerlifting</a>, <a href="https://bandcamp.com/rahulgoel">dance music</a> & cooking</p>
                 <p className="main-page-sub-p  ">Aspiration: become a polymath</p>
