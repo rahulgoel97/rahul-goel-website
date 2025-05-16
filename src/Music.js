0abd3dae92c891233befdb96d70806c9c31acbc7
Rahul Goel <RTBF73@motorolasolutions.com>
2025-05-16 18:39:16 -0500

    Changed phone number and added my latest techno set

--- src/Music.js
+++ src/Music.js
@@ -15,8 +15,12 @@ function Music() {
          <div className="page-title"> 
              Music 🎵
         </div>   
-        <h2 className="music-subhead"> Mixes </h2>
 
+        <p className="main-page-sub-p"> I mix techno, house, and other dance music under the name "bigOther", inspired by Jaques Lacan </p>
+
+
+        <h2 className="music-subhead"> Mixes </h2>
+        <iframe className="soundcloud-embed" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2020738941&color=%230066cc&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">2Hr set</iframe>
         <iframe className="soundcloud-embed" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1130995420&color=%230066cc&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">2Hr set</iframe>
         <iframe className="soundcloud-embed" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1133822788&color=%230066cc&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">2Hr set</iframe>
         <iframe className="soundcloud-embed" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/334374143&color=%230066cc&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">2Hr set</iframe>
