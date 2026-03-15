9323d8879c79c0b0ec06c8b79f350b839ef86664
Rahul Goel <RTBF73@motorolasolutions.com>
2026-03-15 12:39:58 -0500

    Edited links

--- src/Home.js
+++ src/Home.js
@@ -27,11 +27,11 @@ function Home() {
 
             <div className="main-page-p-box">
                 <p className="main-page-sub-contact"> (773) 837 1963 | rahulgol97@gmail.com </p>
-                <p className="main-page-sub-p"> I'm a strategy professional based in Chicago, currently at Motorola Solutions. I focus on research & quantitative analysis for strategic projects and tech M&A. </p>
-                <p className="main-page-sub-p  "> I grew up in New Delhi & Mumbai, studied engineering at Northwestern University and have lived in Chicago since 2015. I often think about how technology can dramatically reduce cost, improve access and forge a path for self-actualization for everyone.  </p>
+                <p className="main-page-sub-p"> Strategist at the intersection of tech M&A and product.  </p>
+                <p className="main-page-sub-p  "> I studied engineering at Northwestern University and have lived in Chicago since 2015. I often think about how data and technology can keep people safe, improve access and forge a path for self-actualization for everyone.  </p>
                 <p className="main-page-sub-p  ">Interests: <a href="https://www.instagram.com/p/C2Tird-rOqa/">powerlifting</a>, <a href="https://bandcamp.com/rahulgoel">dance music</a> & cooking</p>
-                <p className="main-page-sub-p  ">Aspiration: become a polymath</p>
-                <p className="main-page-sub-p  ">Building: apps to learn about technology, make my life easier & have some fun</p>
+                <p className="main-page-sub-p  ">Building: Apps for <a href="https://apps.apple.com/al/app/stresscalc-rpe-calculator/id1587780631">powerlifting</a>, finance and music</p>
+                <p className="main-page-sub-p  ">I document my thoughts on books, finance, and technology on my <a href="https://rahulsdojo.substack.com/">blog</a></p>
             </div>
 
         </div>     
